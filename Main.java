5a779d6ab0d2b03e6e299e478ead746e2d528f2a
olorene <ggarik83@gmail.com>
2020-02-19 17:10:21 +0200

    Work login on site

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+
+        Connection.Response loginForm = Jsoup.connect("https://maxtable.top/admin/")
+                .method(Connection.Method.GET)
+                .execute();
+
+        Document document = Jsoup.connect("https://maxtable.top/admin/index.php?route=common/login")
+//                .data("cookieexists", "false")
+                .data("username", "xxx")
+                .data("password", "xxx")
+                .cookies(loginForm.cookies())
+                .post();
+        System.out.println(document);
+
     }
 }
